3d402818318e8df947463191a8d5c7431bdde2fa
unknown <Ajith.KK@IN2065067W2.ey.net>
2016-04-12 15:05:27 +0530

    adding test file

---
+++ src/com/practic/package1/Test1.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2016 EY LLP.
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of EY LLP. ("Confidential Information").
+ * You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license
+ * agreement you entered into with EY.
+ */
+
+package com.practic.package1;
+
+/**
+ *
+ * @author Ajith.KK
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
